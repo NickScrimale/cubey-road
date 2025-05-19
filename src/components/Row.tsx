a87b3e668c8fcd65fae605dc094ee20dbc0ec238
gamer <bscrinale@gmail.com>
2025-05-19 11:15:18 -0500

    added project to repo

---
+++ src/components/Row.tsx
@@ -0,0 +1,19 @@
+import type { Row } from "../types";
+import { CarLane } from "./CarLane";
+import { Forest } from "./Forest"
+
+type Props = {
+    rowIndex: number;
+    rowData: Row;
+};
+
+export function Row({ rowIndex, rowData }: Props) {
+    switch(rowData.type) {
+        case "forest": {
+            return <Forest rowIndex={rowIndex} rowData={rowData} />;
+        }
+        case "car": {
+            return <CarLane rowIndex={rowIndex} rowData={rowData} />;
+        }
+    }
+}
---
+++ src/metadata.ts
@@ -0,0 +1,10 @@
+import type { Row } from "./types";
+
+export const rows: Row[] = [
+    {
+        type: "car",
+        direction: false,
+        speed: 1,
+        vehicles: [{ initialTileIndex: 2, color: 0xff0000 }],
+    },
+];
