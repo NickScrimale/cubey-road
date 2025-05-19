05317bd8837874e19c4c823da881285ee1046599
gamer <bscrinale@gmail.com>
2025-05-19 11:23:52 -0500

    added entire project

--- src/components/Wheel.tsx
+++
@@ -1,8 +0,0 @@
-export function Wheel({ x }: { x: number }) {
-    return (
-        <mesh position={[x, 0, 6]}>
-            <boxGeometry args={[12, 33, 12]} />
-            <meshLambertMaterial color={0x333333} flatShading />
-        </mesh>
-    );
-}
